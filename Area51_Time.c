e8128ce469d71362226ff3b0747eef66d8185d4f
TheUn-fixed <53077791+TheUn-fixed@users.noreply.github.com>
2019-07-21 12:16:52 +0800

    Military to 12-hour Notation
    Basically, converts military time into its corresponding 12-hour notation.

--- Area51_Time.c
+++ Area51_Time.c
@@ -5,15 +5,18 @@
 #define p printf
 #define s scanf
 
+//Initialization of Integers
 int time_input, hours, minutes;
 
+//Object: Title
 void title()
 {
 	
-	p("Area51 Time\n\n");
+	p("Time in Area 51\n\n");
 	
 }
 
+//Object: Input
 void input()
 {
 	
@@ -22,29 +25,40 @@ void input()
 	
 }
 
+//Object: Time Calculation
 void timecalc()
 {
 	
 	hours = (time_input / 100) - 12;
-	minutes = ((time_input % 100));
+	minutes = (time_input % 100);
+	
+	if (time_input >= 0 && time_input <=1259)
+	{
+		
+		hours = (time_input / 100);
+		minutes = (time_input % 100);	
+		
+	}
+	
 	
 }
 
+//Object: Display Result
 void display()
 {
 	
-	p("\nTime in Military Notation: %d", time_input);
 	
 	if (time_input >= 0 && time_input <= 1159)
 	{
 		
+		p("\nTime in Military Notation: %.4d", time_input);	
 		p("\n\nTime in 12-hr Notation: %.2d:%.2d AM", hours, minutes);	
 		
 	}
 	
 	else if (time_input >= 1200 && time_input <= 2359)
 	{
-		
+		p("\nTime in Military Notation: %.4d", time_input);
 		p("\n\nTime in 12-hr Notation: %.2d:%.2d PM", hours, minutes);	
 		
 	}
@@ -52,7 +66,7 @@ void display()
 	else
 	{
 		
-		p("Input is Invalid!");
+		p("\nInput is Invalid!");
 		
 	}
 	
@@ -60,6 +74,7 @@ void display()
 	
 }
 
+//Main Function
 main()
 {
 	
@@ -71,4 +86,3 @@ main()
 	return 0;
 	
 }
-
